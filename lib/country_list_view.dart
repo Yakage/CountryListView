fb796666d990abf63a268cd1e84f3502f510a83c
Mark Benedict Abalos <benedictabalos30@gmail.com>
2024-08-02 23:15:46 +0800

    Delete the debug flag and fix the paddings

--- lib/country_list_view.dart
+++ lib/country_list_view.dart
@@ -31,50 +31,49 @@ class _CountryListViewState extends State<CountryListView> {
         elevation: 0.0,
       ),
       body: Center(
-        child: Container(
-          margin: const EdgeInsets.all(30),
-          child: ListView.separated(
+          child: Container(
+            margin: const EdgeInsets.all(30),
+            child: ListView.separated(
             itemCount: countries.length,
-            separatorBuilder: (context, index) => const SizedBox(height: 25,),
+            separatorBuilder: (context, index) => const SizedBox(
+              height: 25,
+            ),
             itemBuilder: (context, index) {
-              return Container(
-                padding: const EdgeInsets.all(15),
-                decoration: BoxDecoration(
+            return Container(
+              padding: const EdgeInsets.all(15),
+              decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(10),
                   border: Border.all(
                     color: Colors.black,
                     width: 1,
-                  )
-                ),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
+                  )),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 20, 30, 20),
+                      child: Image.asset(
+                        countries[index].flagIcon,
+                        width: 70,
+                        height: 70,
+                        fit: BoxFit.cover,
+                      )),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 20, 60, 20),
-                        child: Image.asset(
-                          countries[index].flagIcon,
-                          width: 50,
-                          height: 50,
-                          fit: BoxFit.cover,
-                        )
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Country Name: ${countries[index].countryName}"),
-                          Text("Language: ${countries[index].language}"),
-                          Text("Continent: ${countries[index].continent}"),
-                        ],
-                      )
+                      Text("Country Name: ${countries[index].countryName}"),
+                      Text("Language: ${countries[index].language}"),
+                      Text("Continent: ${countries[index].continent}"),
                     ],
-                ),
-              );
-            },
-          ),
-        )
-      ),
+                  )
+                ],
+              ),
+            );
+          },
+        ),
+      )),
     );
   }
 }
